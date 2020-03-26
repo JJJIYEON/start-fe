8ec322fd035abc56b8e7af612404590761964942
JJJIYEON <cute2nu@gmail.com>
2020-03-26 19:07:41 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,15 @@
+console.log('app.js');
+
+var age = 20;
+console.log(age);
+
+var isMan = true;
+console.log(isMan);
+
+var month = ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월", {} ];
+console.log(month);
+
+var image = {name:'1.png',}; // image = new Object();
+image.width = 100;
+image.height = 200;
+console.log(image);
